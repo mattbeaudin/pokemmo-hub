f47ce46a53290903f2fd163e6594647377b42549
Matt Beaudin <beaudinm@msu.edu>
2024-07-08 01:35:04 -0400

    Added Desktop Notification for berry timer.

--- src/components/Berries/BerryAccountItem.jsx
+++ src/components/Berries/BerryAccountItem.jsx
@@ -13,6 +13,31 @@ import { BerryTimeChanger } from './BerryTimeChanger';
 const DRY_FLOOR_TIME1 = -10;
 const DRY_FLOOR_TIME2 = -15;
 
+function notifyMe(notificationText) {
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification(notificationText);
+      // …
+    } else if (Notification.permission !== "denied") {
+        // We need to ask the user for permission
+        Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                const notification = new Notification(notificationText);
+                // …
+            }
+        });
+    }
+  
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+}
+  
+
 const getDropletColor = (hours, limit, isDarkMode, isLongGrowth) => {
     if (!isLongGrowth && hours <= DRY_FLOOR_TIME1) {
         return { color: "red", className: "blinking-droplet" }
@@ -42,6 +67,10 @@ export const BerryAccountItem = (plantedBerry) => {
         ? diffTimestamp(berry.tsLastWater, now)
         : diffTimestamp(berry.tsLastWater - getMsFromHour(6), now, true);
     const timeToReady = diffTimestamp(berry.tsPlant + getMsFromHour(berry.grow_time), now)
+
+    if (timeToReady.minutes === 1) {
+        notifyMe("Your berries are ready to be harvested!")
+    }
 
     useEffect(() => {
         setInterval(() => {
